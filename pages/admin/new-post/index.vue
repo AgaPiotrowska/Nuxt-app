e2932a500b9adda90091b27b0077f693c47144e8
Aga <agniegrabo@gmail.com>
2021-01-14 20:49:33 +0100

    plugins added

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -8,13 +8,8 @@
 
 <script>
   import axios from 'axios'
-  import AdminPostForm from "@/components/Admin/AdminPostForm";
-
   export default {
     layout: 'admin',
-    components: {
-      AdminPostForm
-    },
     methods: {
       onSubmitted(postData) {
         this.$store.dispatch('addPost', postData)
