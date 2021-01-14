e6e0a8d9ff5efb243a4f23e04764df4c5bf7c9be
Aga <agniegrabo@gmail.com>
2021-01-14 13:28:10 +0100

    initializing store

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -2,10 +2,13 @@
   <form @submit.prevent="onSave">
     <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
     <AppControlInput v-model="editedPost.title">Title</AppControlInput>
-    <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+    <AppControlInput v-model="editedPost.thumbnail">Thumbnail Link</AppControlInput>
     <AppControlInput
       control-type="textarea"
       v-model="editedPost.content">Content</AppControlInput>
+    <AppControlInput
+      control-type="textarea"
+      v-model="editedPost.previewText">PreviewText</AppControlInput>
     <AppButton type="submit">Save</AppButton>
     <AppButton
       type="button"
@@ -37,8 +40,9 @@
           : {
             author: "",
             title: "",
-            thumbnailLink: "",
-            content: ""
+            thumbnail: "",
+            content: "",
+            previewText: ""
           }
       };
     },
